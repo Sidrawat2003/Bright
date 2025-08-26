d260105416f62ef3e5f6c45d5d1da1bfa2a1d896
Siddhant <siddhantsocool10@gmail.com>
2025-08-26 14:21:21 +0530

    Variables

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
-console.log("Hello")
-console.log("hey")
+const accountID = 144553
+let accountEmail = "itachi@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountID = 2 // not allowed
+
+accountEmail = "sid@google.com"
+accountPassword = "Lucknow"
+
+console.log(accountID);
+
+console.table([accountEmail, accountID, accountPassword, accountCity])
+
+
+
